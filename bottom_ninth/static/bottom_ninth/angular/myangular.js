0cc03cf06e3fc7bc1eb60330d689b02393e68461
tom.louis.murphy <tom.louis.murphy@gmail.com>
2017-12-05 15:27:16 -0600

    midday update

--- bottom_ninth/static/bottom_ninth/angular/myangular.js
+++ bottom_ninth/static/bottom_ninth/angular/myangular.js
@@ -2,6 +2,21 @@ console.log('testing');
 
 const app = angular.module('MyApp', ['ngRoute']);
 
+app.config(['$locationProvider', function($locationProvider){
+	$locationProvider.html5Mode({ enabled: true });
+}]);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+	$routeProvider
+		.when('/', {
+			templateUrl: '',
+			controller: 'MainController'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+}]);
+
 app.controller('MainController', [function(){
 	this.test = 'test';
 }])
